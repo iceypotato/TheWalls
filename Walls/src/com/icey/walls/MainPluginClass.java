99ea3fd20d8787701fe46fd1ff49bc82276497b6
iceypotato <niceawlego@gmail.com>
2020-05-18 09:36:56 -0700

    Walls help sucessful

--- Walls/src/com/icey/walls/MainPluginClass.java
+++ Walls/src/com/icey/walls/MainPluginClass.java
@@ -1,18 +1,20 @@
 package com.icey.walls;
 
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.icey.walls.commands.Info;
 
 public class MainPluginClass extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
 		getLogger().info("Walls 1.0 has been enabled.");
-		getServer().getConsoleSender().sendMessage("Walls 1.0 has been enabled.");
+		this.getCommand("walls").setExecutor((CommandExecutor) new Info(this));
 	}
 	@Override
 	public void onDisable() {
 		getLogger().info("Walls 1.0 has been disabled.");
-		getServer().getConsoleSender().sendMessage("Walls 1.0 has been disabled.");
 	}
 
 }
--- Walls/src/com/icey/walls/commands/Info.java
+++ Walls/src/com/icey/walls/commands/Info.java
@@ -5,20 +5,30 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.ChatPaginator;
 
 import com.icey.walls.MainPluginClass;
 
 public class Info implements CommandExecutor {
 	private MainPluginClass myplugin;
-	
+
 	public Info(MainPluginClass mainClass) {
 		myplugin = mainClass;
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		Player player = (Player) sender;
-		sender.sendMessage(ChatColor.GOLD+"Running Walls Minigame Plugin 1.0");
+		if (args.length > 0) {
+			if (args[0].equals("help")) {
+				sender.sendMessage(ChatColor.GOLD + "Walls Help Placeholder");
+			}
+			else {
+				sender.sendMessage(ChatColor.RED + "Incorrect Argument! " + ChatColor.GREEN + "do /walls help for the list of commands.");
+			}
+		}
+		else {
+			sender.sendMessage(ChatColor.GOLD + "Running Walls Minigame Plugin 1.0. Type /walls help for more.");
+		}
 		return true;
 	}
 
