2402f08188228b9044a8983499eaef0e1d200f4a
iceypotato <niceawlego@gmail.com>
2020-05-20 13:38:26 -0700

    Got wand to work

--- Walls/src/com/icey/walls/MainPluginClass.java
+++ Walls/src/com/icey/walls/MainPluginClass.java
@@ -9,6 +9,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.icey.walls.commands.Walls;
+import com.icey.walls.listeners.WallsTool;
 
 public class MainPluginClass extends JavaPlugin {
 
@@ -20,6 +21,7 @@ public class MainPluginClass extends JavaPlugin {
 	public void onEnable() {
 		getLogger().info("Walls 1.0 has been enabled.");
 		this.getCommand("walls").setExecutor((CommandExecutor) new Walls(this));
+		getServer().getPluginManager().registerEvents(new WallsTool(), this);
 		loadConfig();
 	}
 	@Override
--- Walls/src/com/icey/walls/listeners/WallsTool.java
+++ Walls/src/com/icey/walls/listeners/WallsTool.java
@@ -3,17 +3,26 @@ package com.icey.walls.listeners;
 import java.util.ArrayList;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class WallsTool implements Listener {
 	
-	public void giveTool(Player p) {
-		ItemStack item = new ItemStack(Material.BLAZE_ROD);
-		ItemMeta meta = item.getItemMeta();
+	ItemStack item;
+	ItemMeta meta;
+	Player player;
+	
+	public WallsTool() {
+		item = new ItemStack(Material.BLAZE_ROD);
+		meta = item.getItemMeta();
 		meta.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Walls Tool");
 		ArrayList<String> lore = new ArrayList<String>();
 		lore.add(ChatColor.DARK_PURPLE + "Left-Click to set the first corner of a region,");
@@ -21,6 +30,38 @@ public class WallsTool implements Listener {
 		lore.add(ChatColor.DARK_PURPLE + "Left-Click to set the spawnpoint for lobbies or team spawns.");
 		meta.setLore(lore);
 		item.setItemMeta(meta);
+	}
+	
+	public void giveTool(Player p) {
 		p.getInventory().addItem(item);
+	}
+	
+	@EventHandler
+	public void onClick(PlayerInteractEvent event) {
+		player = event.getPlayer();
+		ItemStack item = player.getItemInHand();
+
+		if (item.equals(this.item)) {
+			Block block = event.getClickedBlock();
+			if (block != null) {
+				Player player = event.getPlayer();
+				Location loc = block.getLocation();
+				Action action = event.getAction();
+				if (action == Action.LEFT_CLICK_BLOCK) {
+					player.sendMessage(ChatColor.LIGHT_PURPLE + "Region #1:");
+					player.sendMessage(ChatColor.WHITE + "X: " + ChatColor.GREEN + loc.getBlockX());
+					player.sendMessage(ChatColor.WHITE + "Y: " + ChatColor.GREEN + loc.getBlockY());
+					player.sendMessage(ChatColor.WHITE + "Z: " + ChatColor.GREEN + loc.getBlockZ());
+					event.setCancelled(true);
+				}
+				else if (action == Action.RIGHT_CLICK_BLOCK) {
+					player.sendMessage(ChatColor.LIGHT_PURPLE + "Region #2:");
+					player.sendMessage(ChatColor.WHITE + "X: " + ChatColor.GREEN + loc.getBlockX());
+					player.sendMessage(ChatColor.WHITE + "Y: " + ChatColor.GREEN + loc.getBlockY());
+					player.sendMessage(ChatColor.WHITE + "Z: " + ChatColor.GREEN + loc.getBlockZ());
+					event.setCancelled(true);
+				}
+			}
+		}
 	}
 }
