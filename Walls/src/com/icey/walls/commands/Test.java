df654d2dc17b1882d55f98fbfb1a32cb8f52fea3
iceypotato <niceawlego@gmail.com>
2021-04-05 09:49:27 -0700

    Working on friendly fire

--- Walls/src/com/icey/walls/commands/Test.java
+++ Walls/src/com/icey/walls/commands/Test.java
@@ -1,18 +1,16 @@
 package com.icey.walls.commands;
 
 import java.util.Iterator;
-import java.util.Random;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.Team;
 
 import com.icey.walls.MainClass;
+import com.icey.walls.framework.ScoreboardSharedTeams;
 import com.icey.walls.listeners.WallsTool;
 import com.icey.walls.versions.v1_8_R3.WallsScoreboardSharedTeams;
 
@@ -20,21 +18,38 @@ public class Test implements CommandExecutor {
 	
 	private MainClass myplugin;
 	private WallsTool wallsTool;
-	WallsScoreboardSharedTeams sharedteams = new WallsScoreboardSharedTeams();
+	ScoreboardSharedTeams sharedteams;
 	
 	public Test(MainClass main, WallsTool wallsTool) {
 		this.myplugin = main;
 		this.wallsTool = wallsTool;
+		sharedteams = myplugin.getNewScoreboardSharedTeams();
 	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (args.length > 0) {
 			Player player = (Player) sender;
-			if (args[0].equalsIgnoreCase("rand")) {
-				for(String msg : myplugin.arenaManager.getDeathMsgStrings()) {
-					player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+			if (args[0].equalsIgnoreCase("getsb")) {
+				Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
+				sharedteams.addTeamsToScoreboard(board);
+				player.setScoreboard(board);
+			}
+			else if (args[0].equalsIgnoreCase("rmsb")) {
+				player.setScoreboard(Bukkit.getScoreboardManager().getMainScoreboard());
+			}
+			else if (args[0].equalsIgnoreCase("joinred")) {
+				sharedteams.joinTeam(player, "red");
+			}
+			else if (args[0].equalsIgnoreCase("joinblue")) {
+				sharedteams.joinTeam(player, "blue");
+			}
+			else if (args[0].equalsIgnoreCase("list")) {
+				for (Player p : Bukkit.getOnlinePlayers()) {
+					Bukkit.getConsoleSender().sendMessage("Scoreboard: " + p.getScoreboard());
+					Bukkit.getConsoleSender().sendMessage("Teams: " + p.getScoreboard().getTeams()+"");
 				}
+				
 			}
 			else {
 				sender.sendMessage("Invalid subcommand.");
